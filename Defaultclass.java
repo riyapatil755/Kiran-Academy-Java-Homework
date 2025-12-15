e1d468ea2248a0166f18eaeab49cae401863ffa1
riyapatil755 <riyabhagwanpatil75@gmail.com>
2025-12-15 18:00:22 +0530

    Add Defaultclass with default variables

---
+++ Defaultclass.java
@@ -0,0 +1,13 @@
+package com.kiranacademy.homework;
+
+public class Defaultclass {
+  int v1;
+  float v2;
+  double v3;
+  byte v4;
+  char v5;
+  short v6;
+  long v7;
+  boolean v8;
+  String v9;
+}
