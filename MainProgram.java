091a033246050c1b3f468e780b63ac4e206ec690
riyapatil755 <riyabhagwanpatil75@gmail.com>
2025-12-15 18:01:15 +0530

    Add MainProgram to access Defaultclass
    Implement main program to display variables from Defaultclass

---
+++ MainProgram.java
@@ -0,0 +1,18 @@
+package com.kiranacademy.homework;
+
+public class MainProgram {
+    public static void main(String[] args) {
+
+        Defaultclass d = new Defaultclass(); 
+
+        System.out.println(d.v1);
+        System.out.println(d.v2);
+        System.out.println(d.v3);
+        System.out.println(d.v4);
+        System.out.println(d.v5);
+        System.out.println(d.v6);
+        System.out.println(d.v7);
+        System.out.println(d.v8);
+        System.out.println(d.v9);
+    }
+}
